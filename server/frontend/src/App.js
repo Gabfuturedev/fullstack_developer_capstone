afb1169736ed3075b0b514cf50f25a8a8e0f6856
Gab <whathe201@gmail.com>
2026-03-10 09:13:22 +0800

    feat: about urls

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,11 @@
 import LoginPanel from "./components/Login/Login"
+import Home from "./components/Home/Home"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
